99a347934cabb4838592200a593c4f18a8c54edf
intelia <Intelia@163.com>
2020-07-10 18:48:09 +0800

    Initial commit

---
+++ src/main/java/com/intelia/jianfa.java
@@ -0,0 +1,16 @@
+package com.intelia;
+
+/**
+ * @description TODO
+ * @auther Intelia
+ * @date 2020.7.10 18:45
+ * @mogified By:
+ */
+public class jianfa {
+    public static void main(String[] args) {
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+    }
+}
