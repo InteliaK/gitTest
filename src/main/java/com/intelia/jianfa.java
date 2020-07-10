fa3160cd9640af3a5323df399a7bd70674656c6d
intelia <intelia@163.com>
2020-07-10 19:23:17 +0800

    独孤九剑

--- src/main/java/com/intelia/jianfa.java
+++ src/main/java/com/intelia/jianfa.java
@@ -12,5 +12,9 @@ public class jianfa {
         System.out.println("独孤九剑");
         System.out.println("独孤九剑");
         System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
     }
 }
