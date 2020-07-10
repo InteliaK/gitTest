dbd680b628d0a84f96d5875290b23538e117a602
intelia <intelia@163.com>
2020-07-10 19:33:12 +0800

    独孤九剑

--- src/main/java/com/intelia/jianfa.java
+++ src/main/java/com/intelia/jianfa.java
@@ -16,5 +16,10 @@ public class jianfa {
         System.out.println("独孤九剑");
         System.out.println("独孤九剑");
         System.out.println("独孤九剑");
+
+        //红色 未追踪代码
+        //绿色 暂存区
+        //黑色 已提交本地库
+        //蓝色 已提价本地库后，又发生了修改切未提交
     }
 }
