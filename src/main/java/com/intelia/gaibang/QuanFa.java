47e03f045f661d9574f7a9dadf5b77b939ea9f90
lin <66442819@qq.com>
2020-07-13 15:06:55 +0800

    updata 0.0.5

---
+++ src/main/java/com/intelia/gaibang/QuanFa.java
@@ -0,0 +1,13 @@
+package com.intelia.gaibang;
+
+/**
+ * @description TODO
+ * @auther Intelia
+ * @date 2020.7.13 15:05
+ * @mogified By:
+ */
+public class QuanFa {
+    public static void main(String[] args) {
+        System.out.println("睡梦罗汉拳");
+    }
+}
