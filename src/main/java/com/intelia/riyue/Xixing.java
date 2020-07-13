98b9e3c7b7024a6f2ddda5e60c121525d4c8fd06
lin <66442819@qq.com>
2020-07-13 14:51:06 +0800

    update 0.0.3

---
+++ src/main/java/com/intelia/riyue/Xixing.java
@@ -0,0 +1,16 @@
+package com.intelia.riyue;
+
+/**
+ * @description TODO
+ * @auther Intelia
+ * @date 2020.7.13 14:48
+ * @mogified By:
+ */
+public class Xixing {
+    public static void main(String[] args) {
+        System.out.println("吸星大法好");
+        System.out.println("吸星大法好");
+        System.out.println("吸星大法好");
+        System.out.println("吸星大法好");
+    }
+}
